3237c6bdfe5cc9bc68858a89169deda23d36277d
Snyder <asnyder@usgs.gov>
2021-09-17 12:36:11 -0400

    update fetch functions

--- 1_fetch/src/get_nwis_data.R
+++ 1_fetch/src/get_nwis_data.R
@@ -24,12 +24,13 @@ nwis_site_info <- function(fileout, site_data){
 }
 
 
-download_nwis_site_data <- function(filepath, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
+download_nwis_site_data <- function(site_num, parameterCd = '00010', startDate="2014-05-01", endDate="2015-05-01"){
   
-  # filepaths look something like directory/nwis_01432160_data.csv,
-  # remove the directory with basename() and extract the 01432160 with the regular expression match
-  site_num <- basename(filepath) %>% 
-    stringr::str_extract(pattern = "(?:[0-9]+)")
+  # Create tmp data directory
+  tmp_output_dir =  file.path('1_fetch', 'tmp')
+  dir.create(tmp_output_dir, showWarnings = FALSE)
+  
+  filepath <- file.path(tmp_output_dir, paste0('nwis_', site_num, '_data.csv'))
   
   # readNWISdata is from the dataRetrieval package
   data_out <- readNWISdata(sites=site_num, service="iv", 
@@ -46,3 +47,14 @@ download_nwis_site_data <- function(filepath, parameterCd = '00010', startDate="
   return(filepath)
 }
 
+concat_data <- function(tmp_output_dir){
+  download_files <-list.files(tmp_output_dir)
+  data_out <- data.frame()
+  # loop through files to download 
+  for (download_file in download_files){
+    # read the downloaded data and append it to the existing data.frame
+    these_data <- read_csv(file.path(tmp_output_dir, download_file), col_types = 'ccTdcc')
+    data_out <- bind_rows(data_out, these_data)
+  }
+  return(data_out)
+}
